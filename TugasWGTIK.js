07a18b70b067ee9c14977bb183e4ca06ddcc4903
rrxfanda <89523468+rrxfanda@users.noreply.github.com>
2024-06-02 20:44:15 +0700

    Create TugasWGTIK.js & Menambahkan Beberapa Kondisi
    Membuat fungsi kenaRazia yang berisi parameter date untuk merepresentasikan tanggal & data yang nantinya berisi data kendaraan serta menambahkan beberapa kondisi untuk melakukan pengecekan tanggal ganjil/genap dan memasukkan daftar rute yang dibatasi

---
+++ TugasWGTIK.js
@@ -0,0 +1,16 @@
+function kenaRazia(date, data) {
+    const isDateOdd = date % 2 !== 0;
+    
+    const restrictedRoutes = [
+      "Gajah Mada",
+      "Hayam Wuruk",
+      "Sisingamangaraja",
+      "Panglima Polim",
+      "Fatmawati",
+      "Tomang Raya"
+    ];
+  
+    const result = [];
+
+    return result;
+  }
